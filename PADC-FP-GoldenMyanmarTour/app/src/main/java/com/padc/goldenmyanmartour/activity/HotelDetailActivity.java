ae2aef39636753101c1d6e45db2a6f19a2fa2a1a
HtetHtetWin91 <htethtetwin.minkin@gmail.com>
2016-09-16 14:23:56 -0700

    -add address/ph icon and update textSize in view_item_hotel.xml -set GridLayoutManger as 1 in HotelFragment.java -add ExpandableListAdapter.java -add list_item.xml and list_group.xml for ExpandableList -add prepareListData() method in HotelDetailActivity.java -add ExpandableListView in acitvity_hotel_detail.xml

--- PADC-FP-GoldenMyanmarTour/app/src/main/java/com/padc/goldenmyanmartour/activity/HotelDetailActivity.java
+++ PADC-FP-GoldenMyanmarTour/app/src/main/java/com/padc/goldenmyanmartour/activity/HotelDetailActivity.java
@@ -9,14 +9,22 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.BaseAdapter;
+import android.widget.ExpandableListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.padc.goldenmyanmartour.GMTApp;
 import com.padc.goldenmyanmartour.R;
+import com.padc.goldenmyanmartour.adapters.ExpandableListAdapter;
 import com.padc.goldenmyanmartour.adapters.ImagesPagerAdapter;
 import com.padc.goldenmyanmartour.components.PageIndicatorView;
 import com.padc.goldenmyanmartour.data.vo.HotelVO;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -34,8 +42,8 @@ public class HotelDetailActivity extends BaseActivity {
     @BindView(R.id.fab)
     FloatingActionButton fab;
 
-    @BindView(R.id.tv_hotel_desc)
-    TextView tvHotelDesc;
+//    @BindView(R.id.tv_hotel_desc)
+//    TextView tvHotelDesc;
 
     @BindView(R.id.pager_hotel_images)
     ViewPager pagerHotelImages;
@@ -47,8 +55,23 @@ public class HotelDetailActivity extends BaseActivity {
     @BindView(R.id.pi_hotel_image_slider)
     PageIndicatorView piHotelImageSlider;
 
+
+    @BindView(R.id.lvExp)
+    ExpandableListView lvExpander;
+
     private String mHotelName;
     private HotelVO mHotel;
+
+    //Expandable List View
+    ExpandableListAdapter listAdapter;
+
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+
+
+
+
+
 
 
     public static Intent newIntent(String hotelName) {
@@ -70,13 +93,127 @@ public class HotelDetailActivity extends BaseActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(this,listDataHeader,listDataChild);
+
+        lvExpander.setAdapter(listAdapter);
+
         mHotelName = getIntent().getStringExtra(IE_HOTEL_NAME);
 
         bindData();
+
+        //expandable list
+
+
+
+
+        // preparing list data
+
+
+//        List<String> top250 = new ArrayList<String>();
+//        top250.add("The Shawshank Redemption");
+//        top250.add("The Godfather");
+//        top250.add("The Godfather: Part II");
+//        top250.add("Pulp Fiction");
+//        top250.add("The Good, the Bad and the Ugly");
+//        top250.add("The Dark Knight");
+//        top250.add("12 Angry Men");
+
+
+        // setting list adapter
+
+
+//        // Listview Group click listener
+//        lvExpander.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+//
+//            @Override
+//            public boolean onGroupClick(ExpandableListView parent, View v,
+//                                        int groupPosition, long id) {
+//                // Toast.makeText(getApplicationContext(),
+//                // "Group Clicked " + listDataHeader.get(groupPosition),
+//                // Toast.LENGTH_SHORT).show();
+//                return false;
+//            }
+//        });
+
+        // Listview Group expanded listener
+
+
     }
 
+
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("Accomodation");
+        listDataHeader.add("Restaurant");
+        listDataHeader.add("Facilities&Services");
+
+
+        // Adding child data
+        List<String> accomodation = new ArrayList<String>();
+        accomodation.add("Superior Room  ->   26.36m");
+        accomodation.add("Ondol Room    ->   26.36m");
+        accomodation.add("Premier Executive Room (Sea View) ->  26.36m");
+        accomodation.add("Deluxe Room    ->   34m ");
+
+
+
+
+        List<String> restaurant = new ArrayList<String>();
+        restaurant.add("Breakfast   ->   6:30~11:30 \n Lunch   ->   12:00~15:00 \n Dinner   ->   17:30~22:00");
+        restaurant.add("CoffeeShop  ->   7:00~23:00");
+        restaurant.add("Barking     ->   6:30~22:00");
+
+
+        List<String> facilities = new ArrayList<String>();
+        facilities.add("Check-in : 3pm  Check-out :12 noon");
+        facilities.add("HighSpeed Internet Access");
+        facilities.add("Parking");
+        facilities.add("Beauty Shop,FitnessRoom/Gym");
+        facilities.add("Swimming Pool");
+        facilities.add("Cash Machine/ATM");
+
+
+
+
+        listDataChild.put(listDataHeader.get(0), accomodation); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), restaurant);
+        listDataChild.put(listDataHeader.get(2), facilities);
+
+
+
+
+//        lvExpander.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+//
+//            @Override
+//            public void onGroupExpand(int groupPosition) {
+//                Toast.makeText(getApplicationContext(),
+//                        listDataHeader.get(groupPosition) + " Expanded",
+//                        Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        // Listview Group collasped listener
+//        lvExpander.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+//
+//            @Override
+//            public void onGroupCollapse(int groupPosition) {
+//                Toast.makeText(getApplicationContext(),
+//                        listDataHeader.get(groupPosition) + " Collapsed",
+//                        Toast.LENGTH_SHORT).show();
+//
+//            }
+//        });
+
+    }
+
+
     private void bindData() {
-        tvHotelDesc.setText(R.string.hotel_desc);
+        //tvHotelDesc.setText(R.string.hotel_desc);
 
         piHotelImageSlider.setNumPage(2);
         String[] images = {"R.drawable.ygnhotel_1", "R.drawable.ygnhotel_2"};
@@ -99,6 +236,6 @@ public class HotelDetailActivity extends BaseActivity {
             }
         });
 
-        collapsingToolbar.setTitle(mHotelName);
+        collapsingToolbar.setTitle("Park Royal Yangon");
     }
 }
