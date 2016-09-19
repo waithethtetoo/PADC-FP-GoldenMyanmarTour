aa89c335d04abec17d7264ccc9a53beae6cf97d7
Mobile <Mobile>
2016-09-19 14:56:00 +0630

    change searchview to autocompletetextview at SearchActivity.java;

--- PADC-FP-GoldenMyanmarTour/app/src/main/java/com/padc/goldenmyanmartour/activity/SearchActivity.java
+++ PADC-FP-GoldenMyanmarTour/app/src/main/java/com/padc/goldenmyanmartour/activity/SearchActivity.java
@@ -3,17 +3,20 @@ package com.padc.goldenmyanmartour.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.GridView;
-import android.widget.ListView;
-import android.widget.SearchView;
+import android.widget.ImageView;
+
 
 import com.padc.goldenmyanmartour.GMTApp;
 import com.padc.goldenmyanmartour.R;
 import com.padc.goldenmyanmartour.adapters.DestinationAdapter;
 import com.padc.goldenmyanmartour.adapters.PackageAdapter;
+import com.padc.goldenmyanmartour.data.vo.DestinationVO;
 import com.padc.goldenmyanmartour.views.holders.DestinationViewHolder;
 import com.padc.goldenmyanmartour.views.holders.PackageViewHolder;
 
@@ -27,13 +30,13 @@ public class SearchActivity extends AppCompatActivity {
     private String fragmentName;
 
     @BindView(R.id.searchView)
-    SearchView searchView;
+    AutoCompleteTextView searchView;
+
+    @BindView(R.id.iv_search)
+    ImageView ivSearch;
 
     @BindView(R.id.gv_search_result)
     GridView gvSearchResult;
-
-    @BindView(R.id.lv_search)
-    ListView lvSearch;
 
     DestinationAdapter dAdapter;
     PackageAdapter pAdapter;
@@ -54,81 +57,61 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
         ButterKnife.bind(this, this);
 
-        searchView.setFocusable(true);
-        searchView.requestFocusFromTouch();
-
         fragmentName = getIntent().getStringExtra(IE_SEARCH);
 
         switch (fragmentName) {
             case "Home Fragment":
-                searchView.setQueryHint("Search by destination");
+                            searchView.setHint("Search by destination");
                 // destination list
                 final String[] nameList = {"Yangon", "Mandalay", "Bagan", "Inle Lake", "Golden Rock Pagoda"};
                 ArrayAdapter<String> mAdapter = new ArrayAdapter<String>(GMTApp.getContext(),
-                        android.R.layout.simple_list_item_1, nameList);
-                lvSearch.setAdapter(mAdapter);
-                lvSearch.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        android.R.layout.simple_dropdown_item_1line, nameList);
+                searchView.setThreshold(1);
+                searchView.setAdapter(mAdapter);
+                searchView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        searchHint = lvSearch.getItemAtPosition(position).toString();
-                        searchView.setQuery(searchHint, false); // show ListView selected item
-                        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-                            @Override
-                            public boolean onQueryTextSubmit(String query) {
-//                                searchAction(query);
-                                lvSearch.setVisibility(View.INVISIBLE);
-                                gvSearchResult.setVisibility(View.VISIBLE);
+                        searchHint = parent.getItemAtPosition(position).toString();
+////                        searchAction(searchHint);
+                         /*search function do here */
+                ivSearch.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        gvSearchResult.setVisibility(View.VISIBLE);
 
-                                dAdapter = new DestinationAdapter(null, dController);
-                                gvSearchResult.setAdapter(dAdapter);
-                                return true;
-                            }
-
-                            @Override
-                            public boolean onQueryTextChange(String newText) {
-                                lvSearch.setVisibility(View.VISIBLE);
-                                gvSearchResult.setVisibility(View.INVISIBLE);
-                                return false;
-                            }
-                        });
+                        dAdapter = new DestinationAdapter(null, dController);
+                        gvSearchResult.setAdapter(dAdapter);
                     }
                 });
 
+                    }
+                });
                 break;
 
             case "Package Fragment":
-                searchView.setQueryHint("Search by Destination in Package ");
-
-                // package destination list
-                final String[] destList = {"Yangon", "Bago", "Kyaikhtiyo", "Bagan", "Mt.Popa"};
+                searchView.setHint("Search by package destination");
+                // destination list
+                final String[] destList = {"Yangon", "Mandalay", "Bagan", "Inle Lake", "Golden Rock Pagoda"};
                 ArrayAdapter<String> adapter = new ArrayAdapter<String>(GMTApp.getContext(),
-                        android.R.layout.simple_list_item_1, destList);
-                lvSearch.setAdapter(adapter);
-                lvSearch.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        android.R.layout.simple_dropdown_item_1line, destList);
+                searchView.setThreshold(1);
+                searchView.setAdapter(adapter);
+                searchView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        searchHint = lvSearch.getItemAtPosition(position).toString();
-                        searchView.setQuery(searchHint, false); // show ListView selected item
-                        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                        searchHint = parent.getItemAtPosition(position).toString();
+//                        searchAction(searchHint);
+                        /*search function do here */
+                        ivSearch.setOnClickListener(new View.OnClickListener() {
                             @Override
-                            public boolean onQueryTextSubmit(String query) {
-//                                searchAction(query);
-                                lvSearch.setVisibility(View.INVISIBLE);
+                            public void onClick(View v) {
                                 gvSearchResult.setVisibility(View.VISIBLE);
 
                                 pAdapter = new PackageAdapter(null, pController);
                                 gvSearchResult.setAdapter(pAdapter);
-
-                                return true;
-                            }
-
-                            @Override
-                            public boolean onQueryTextChange(String newText) {
-                                lvSearch.setVisibility(View.VISIBLE);
-                                gvSearchResult.setVisibility(View.INVISIBLE);
-                                return false;
                             }
                         });
+
                     }
                 });
                 break;
@@ -136,20 +119,20 @@ public class SearchActivity extends AppCompatActivity {
     }
 
     // search function do here
-   /* public void searchAction(String query) {
+ /*   public void searchAction(String query) {
+
         String searchQuery = query.toString();
 
         if (searchQuery.equalsIgnoreCase(searchHint)) {
             Log.d("SearchActivity", searchQuery);
-            lvSearch.setVisibility(View.INVISIBLE);
             gvSearchResult.setVisibility(View.VISIBLE);
 
             dAdapter = new DestinationAdapter(null, dController);
             gvSearchResult.setAdapter(dAdapter);
 
-        } else if (searchQuery.equalsIgnoreCase(searchHint)) {
+        }
+        if (searchQuery.equalsIgnoreCase(searchHint)) {
             Log.d("SearchActivity", searchQuery);
-            lvSearch.setVisibility(View.INVISIBLE);
             gvSearchResult.setVisibility(View.VISIBLE);
 
             pAdapter = new PackageAdapter(null, pController);
