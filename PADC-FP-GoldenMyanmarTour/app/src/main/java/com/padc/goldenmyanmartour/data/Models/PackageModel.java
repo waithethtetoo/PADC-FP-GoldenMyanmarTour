d35fd08a6e328fa37fdf60646716f9742d75e96a
Mobile <Mobile>
2016-10-07 08:38:00 +0630

    1. add JsonUtils.java for retrieveing dummy data ; 2. PackageModel.java for showing dummy package data; 3. delete Spinner , MenuItem and add commit line no 130 to 196  because of retrieve data from persistence layer at PackageFragment.java;

--- PADC-FP-GoldenMyanmarTour/app/src/main/java/com/padc/goldenmyanmartour/data/Models/PackageModel.java
+++ PADC-FP-GoldenMyanmarTour/app/src/main/java/com/padc/goldenmyanmartour/data/Models/PackageModel.java
@@ -3,26 +3,36 @@ package com.padc.goldenmyanmartour.data.Models;
 import android.content.Intent;
 import android.support.v4.content.LocalBroadcastManager;
 
+import com.google.gson.reflect.TypeToken;
 import com.padc.goldenmyanmartour.GMTApp;
 import com.padc.goldenmyanmartour.data.vo.PackageVO;
+import com.padc.goldenmyanmartour.events.DataEvent;
+import com.padc.goldenmyanmartour.utils.CommonInstances;
 import com.padc.goldenmyanmartour.utils.DestinationConstants;
+import com.padc.goldenmyanmartour.utils.JsonUtils;
 
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
+import de.greenrobot.event.EventBus;
 
 /**
  * Created by WT on 9/24/2016.
  */
 public class PackageModel extends BaseModel {
 
-    public static final String BROADCAST_DATA_LOADED = "BROADCAST_DATA_LOADED";
+    private static final String DUMMY_PACKAGE_LIST = "package.json";
+
     private static PackageModel model;
     private List<PackageVO> mPackageList;
 
     public PackageModel() {
-        super();
-        mPackageList = new ArrayList<>();
+        mPackageList = initializePackageList();
     }
 
     public static PackageModel getInstance() {
@@ -32,9 +42,24 @@ public class PackageModel extends BaseModel {
         return model;
     }
 
-    public void loadPackages() {
-        dataAgent.loadPackages();
+    private List<PackageVO> initializePackageList() {
+        List<PackageVO> packageList = new ArrayList<>();
+
+        try {
+            String dummyEventList = JsonUtils.getInstance().loadDummyData(DUMMY_PACKAGE_LIST);
+            Type listType = new TypeToken<List<PackageVO>>() {
+            }.getType();
+            packageList = CommonInstances.getInstance().fromJson(dummyEventList, listType);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return packageList;
     }
+//    public void loadPackages() {
+//        dataAgent.loadPackages();
+//    }
 
     public List<PackageVO> getPackageList() {
         return mPackageList;
@@ -49,13 +74,12 @@ public class PackageModel extends BaseModel {
         return null;
     }
 
+    /*
     public void notifyPackagesLoaded(List<PackageVO> packageVOList) {
         mPackageList = packageVOList;
-//        PackageVO.savePackages(mPackageList);
-
-        broadcastPackagesLoadedWithLocalBroadcastManager();
+        PackageVO.savePackages(mPackageList);
+        broadcastDestinationLoadedWithEventBus();
     }
-
 
     public void notifyErrorInLoadingPackages(String message) {
 
@@ -72,9 +96,14 @@ public class PackageModel extends BaseModel {
         return DestinationConstants.IMAGE_ROOT_DIR + packageVO.getPhotos()[packageVO.getPhotos().length - 1];
     }
 
-    private void broadcastPackagesLoadedWithLocalBroadcastManager() {
-        Intent intent = new Intent(BROADCAST_DATA_LOADED);
-        intent.putExtra("key-for-extra", "extra-in-broadcast");
-        LocalBroadcastManager.getInstance(GMTApp.getContext()).sendBroadcast(intent);
+    private void broadcastDestinationLoadedWithEventBus() {
+        EventBus.getDefault().post(new DataEvent.PackageDataLoaded("extra-in-broadcast", mPackageList));
     }
+
+
+    public void setStoredData(List<PackageVO> storedData) {
+        mPackageList = storedData;
+    }
+    */
+
 }
--- PADC-FP-GoldenMyanmarTour/app/src/main/java/com/padc/goldenmyanmartour/fragment/PackageFragment.java
+++ PADC-FP-GoldenMyanmarTour/app/src/main/java/com/padc/goldenmyanmartour/fragment/PackageFragment.java
@@ -34,10 +34,12 @@ import com.padc.goldenmyanmartour.adapters.PackageAdapter;
 
 import com.padc.goldenmyanmartour.data.Models.DestinationModel;
 import com.padc.goldenmyanmartour.data.Models.PackageModel;
+import com.padc.goldenmyanmartour.data.persistence.DestinationContract;
 import com.padc.goldenmyanmartour.data.vo.DestinationVO;
 import com.padc.goldenmyanmartour.data.vo.HotelVO;
 
 import com.padc.goldenmyanmartour.data.vo.PackageVO;
+import com.padc.goldenmyanmartour.data.vo.TourCompanyVO;
 import com.padc.goldenmyanmartour.events.DataEvent;
 import com.padc.goldenmyanmartour.utils.DestinationConstants;
 import com.padc.goldenmyanmartour.views.holders.DestinationViewHolder;
@@ -55,22 +57,14 @@ import de.greenrobot.event.EventBus;
  * Created by WT on 9/6/2016.
  */
 public class PackageFragment extends Fragment {
+
     @BindView(R.id.gv_packages)
     GridView gvPackages;
-
-    @BindView(R.id.sp_filterType)
-    Spinner spFilterType;
 
     private PackageAdapter mAdapter;
     private PackageViewHolder.ControllerItem mController;
 
-    MenuItem packageItemType;
-    MenuItem itemPrice;
     private MenuItemCompat.OnActionExpandListener mOnActionExpandListener;
-
-    public PackageFragment() {
-        setHasOptionsMenu(true);
-    }
 
     public static PackageFragment newInstance() {
         PackageFragment fragment = new PackageFragment();
@@ -80,79 +74,30 @@ public class PackageFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-//        mController = (PackageViewHolder.ControllerItem) context;
+        mController = (PackageViewHolder.ControllerItem) context;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mAdapter = new PackageAdapter(null, mController);
+
+        mAdapter = new PackageAdapter(PackageModel.getInstance().getPackageList(), mController);
+//        List<PackageVO> packageVOList = PackageModel.getInstance().getPackageList();
+//        mAdapter = new PackageAdapter(packageVOList, mController);
+        setHasOptionsMenu(true);
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment_packages, container, false);
         ButterKnife.bind(this, view);
-        gvPackages.setAdapter(mAdapter);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(view.getContext(), R.array.spinner_list_item_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        spFilterType.setAdapter(adapter);
+        gvPackages.setAdapter(mAdapter);
 
         return view;
     }
-
-
-//      @Override
-//       public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-//           inflater.inflate(R.menu.menu_type_filter, menu);
-////           filterItem = menu.findItem(R.id.spinner);
-////           MenuItemCompat.setOnActionExpandListener(filterItem, mOnActionExpandListener);
-//
-//
-//
-//
-//           packageItemType = menu.findItem(R.id.spinner);
-//          packageItemType.setTitle("TourType");
-//           Spinner spinner = (Spinner) MenuItemCompat.getActionView(packageItemType);
-//           ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(GMTApp.getContext(),
-//                   R.array.spinner_list_item_array, android.R.layout.simple_spinner_dropdown_item);
-//           spinner.setAdapter(adapter);
-//           super.onCreateOptionsMenu(menu, inflater);
-//       }
-
-//       @Override
-//
-
-//      @Override
-//       public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-//           inflater.inflate(R.menu.menu_type_filter, menu);
-////           filterItem = menu.findItem(R.id.spinner);
-////           MenuItemCompat.setOnActionExpandListener(filterItem, mOnActionExpandListener);
-//           packageItemType = menu.findItem(R.id.spinner);
-//          packageItemType.setTitle("TourType");
-//           Spinner spinner = (Spinner) MenuItemCompat.getActionView(packageItemType);
-//           ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(GMTApp.getContext(),
-//                   R.array.spinner_list_item_array, android.R.layout.simple_spinner_dropdown_item);
-//           spinner.setAdapter(adapter);
-//           super.onCreateOptionsMenu(menu, inflater);
-//       }
-
-//       @Override
-
-//       public boolean onOptionsItemSelected(MenuItem item) {
-//           int id = item.getItemId();
-//           switch (id) {
-//               case R.id.action_search:
-//                   Toast.makeText(GMTApp.getContext(), "Action filter is clicked", Toast.LENGTH_SHORT).show();
-//                   return true;
-//               case R.id.action_tour_type_filter:
-//                   return true;
-//           }
-//
-//           return super.onOptionsItemSelected(item);
-//       }
 
     @Override
     public void setUserVisibleHint(boolean visible) {
@@ -181,45 +126,72 @@ public class PackageFragment extends Fragment {
         });
     }
 
-//    @Override
-//    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-//        super.onActivityCreated(savedInstanceState);
-//        getActivity().getSupportLoaderManager().initLoader(DestinationConstants.DESTINATION_LIST_LOADER_GRIDVIEW, null, this);
-//    }
-//
-//    @Override
-//    protected void onSendScreenHit() {
-//
-//    }
-//
-//    @Override
-//    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-//        return new CursorLoader(getContext(),
-//                DestinationContract.PackageEntry.CONTENT_URI,
-//                null,
-//                null,
-//                null,
-//                DestinationContract.PackageEntry.COLUMN_PACKAGE_NAME + "ASC");
-//    }
-//
-//    @Override
-//    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-//        List<PackageVO> packageVOList = new ArrayList<>();
-//        if (data != null && data.moveToFirst()) {
-//            do {
-//                PackageVO packageVO = PackageVO.parseFromCursor(data);
-//                packageVO.setPhotos(PackageVO.loadPackageImageByName(packageVO.getPackageName()));
-//                ;
-//                packageVOList.add(packageVO);
-//            } while (data.moveToNext());
-//        }
-//        mAdapter.setNewData(packageVOList);
-//        PackageModel.getInstance().setStoredData(packageVOList);
-//    }
-//
-//    @Override
-//    public void onLoaderReset(Loader<Cursor> loader) {
-//
-//    }
-}
 
+
+/*
+
+    // data retrieve from persistence layer
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(getContext(),
+                DestinationContract.PackageEntry.CONTENT_URI,
+                null,
+                null,
+                null,
+                DestinationContract.PackageEntry.COLUMN_NAME + " ASC");
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        List<PackageVO> packageVOList = new ArrayList<>();
+        if (data != null && data.moveToFirst()) {
+            do {
+                PackageVO packageVO = PackageVO.parseFromCursor(data);
+
+                packageVO.setPhotos(PackageVO.loadPackageImageByName(packageVO.getPackageName()));
+
+                packageVO.setDestinationVOs(DestinationVO.loadDestinationByPackageId(packageVO.getPackageId()));
+
+                packageVO.setTourCompanyVO(TourCompanyVO.loadTourCompanyByPackageID(packageVO.getPackageId()));
+
+                packageVOList.add(packageVO);
+
+            } while (data.moveToNext());
+        }
+
+        mAdapter.setNewData(packageVOList);
+        PackageModel.getInstance().setStoredData(packageVOList);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        EventBus eventBus = EventBus.getDefault();
+        if (!eventBus.isRegistered(this)) {
+            eventBus.register(this);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        EventBus eventBus = EventBus.getDefault();
+        eventBus.unregister(this);
+    }
+
+    public void onEventMainThread(DataEvent.PackageDataLoaded event) {
+        String extra = event.getMessage();
+        Toast.makeText(getContext(), "Extra :" + extra, Toast.LENGTH_SHORT).show();
+
+        List<PackageVO> newPackageList = event.getPackageVOList();
+        mAdapter.setNewData(newPackageList);
+        mAdapter.notifyDataSetChanged();
+    }
+*/
+}
